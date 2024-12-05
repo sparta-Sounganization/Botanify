82eb936e471f2ae7ed8af360f64a3691693abb4c
34-43 <jehyuck3443@gmail.com>
2024-12-05 20:20:38 +0900

    :construction_worker: chore: 불필요 import 제거 #5

--- src/main/java/com/sounganization/botanify/domain/garden/entity/Plant.java
+++ src/main/java/com/sounganization/botanify/domain/garden/entity/Plant.java
@@ -1,11 +1,9 @@
 package com.sounganization.botanify.domain.garden.entity;
 
 import com.sounganization.botanify.common.entity.Timestamped;
-import com.sounganization.botanify.domain.user.entity.User;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
 public class Plant extends Timestamped {
--- src/main/java/com/sounganization/botanify/domain/garden/service/PlantService.java
+++ src/main/java/com/sounganization/botanify/domain/garden/service/PlantService.java
@@ -1,11 +1,8 @@
 package com.sounganization.botanify.domain.garden.service;
 
-import com.sounganization.botanify.domain.garden.dto.PlantRegisterReqDto;
 import com.sounganization.botanify.domain.garden.repository.PlantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
