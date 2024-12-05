f93fb05f9be76dafc8cdc17f0a607776633160a3
34-43 <jehyuck3443@gmail.com>
2024-12-05 18:30:15 +0900

    :sparkles: feat: Auditing 엔티티 'Timestamped' 및 설정 추가 #1

--- src/main/java/com/sounganization/botanify/BotanifyApplication.java
+++ src/main/java/com/sounganization/botanify/BotanifyApplication.java
@@ -2,8 +2,10 @@ package com.sounganization.botanify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BotanifyApplication {
 
     public static void main(String[] args) {
