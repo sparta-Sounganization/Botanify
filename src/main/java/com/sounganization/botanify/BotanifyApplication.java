8a0409e0a49425d8cc4bce0d7d557af62fe72df1
34-43 <jehyuck3443@gmail.com>
2024-12-05 10:03:07 +0900

    :sparkles: #1 feat: Spring Boot 프로젝트 생성 및 테스트

---
+++ src/main/java/com/sounganization/botanify/BotanifyApplication.java
@@ -0,0 +1,13 @@
+package com.sounganization.botanify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotanifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotanifyApplication.class, args);
+    }
+
+}
