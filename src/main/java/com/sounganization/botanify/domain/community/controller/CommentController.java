f8461e2f079b8b7774a42abe223f195563ba13f2
soung90 <sounglikane@gmail.com>
2024-12-06 22:53:43 +0900

    feat: Comment Controller 댓글 생성 API 구현 #8

--- src/main/java/com/sounganization/botanify/domain/community/controller/CommentController.java
+++ src/main/java/com/sounganization/botanify/domain/community/controller/CommentController.java
@@ -1,21 +1,32 @@
 package com.sounganization.botanify.domain.community.controller;
 
-import com.sounganization.botanify.domain.garden.dto.req.PlantReqDto;
+import com.sounganization.botanify.domain.community.dto.req.CommentReqDto;
+import com.sounganization.botanify.domain.community.dto.res.CommentResDto;
+import com.sounganization.botanify.domain.community.service.CommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/comments")
 @RequiredArgsConstructor
 public class CommentController {
 
-    @PostMapping
-    public ResponseEntity<?> test(@Valid @RequestBody PlantReqDto request) {
-        return ResponseEntity.ok().build();
+    private final CommentService commentService;
+
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<CommentResDto> createComment(
+            @PathVariable Long postId,
+            @Valid @RequestBody CommentReqDto requestDto,
+            @RequestParam Long userId // 임시로 더미 userId를 받음
+    ) {
+
+        CommentResDto responseDto = commentService.createComment(postId, requestDto, userId);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(responseDto);
     }
 }
