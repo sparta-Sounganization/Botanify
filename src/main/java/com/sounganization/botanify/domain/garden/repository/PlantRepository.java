be9745fd61b8cbe2a176938704fff372d4d31d01
34-43 <jehyuck3443@gmail.com>
2024-12-06 18:48:51 +0900

    feat: 'Diary' 서비스에 필요한 default 메서드 PlantRepository 에 추가 #11

--- src/main/java/com/sounganization/botanify/domain/garden/repository/PlantRepository.java
+++ src/main/java/com/sounganization/botanify/domain/garden/repository/PlantRepository.java
@@ -1,7 +1,18 @@
 package com.sounganization.botanify.domain.garden.repository;
 
+import com.sounganization.botanify.common.exception.CustomException;
+import com.sounganization.botanify.common.exception.ExceptionStatus;
 import com.sounganization.botanify.domain.garden.entity.Plant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlantRepository extends JpaRepository<Plant, Long> {
+
+    Optional<Plant> findByIdAndDeletedYnFalse(Long id);
+
+    default Plant findByIdCustom(Long id) {
+        return this.findByIdAndDeletedYnFalse(id).orElseThrow(() -> new CustomException(ExceptionStatus.PLANT_NOT_FOUND));
+    }
+
 }
