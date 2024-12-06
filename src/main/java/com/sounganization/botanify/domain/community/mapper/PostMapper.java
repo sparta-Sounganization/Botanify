9bf0b9f20e8247a97a1b0f139e499bed759e5db8
JIMINJI1 <mj013031@gmail.com>
2024-12-06 17:04:41 +0900

    feat: Mapper 인터페이스 추가 및 MapStruct 의존성 추가 #9

---
+++ src/main/java/com/sounganization/botanify/domain/community/mapper/PostMapper.java
@@ -0,0 +1,26 @@
+package com.sounganization.botanify.domain.community.mapper;
+
+import com.sounganization.botanify.domain.community.dto.req.PostReqDto;
+import com.sounganization.botanify.domain.community.dto.res.PostResDto;
+import com.sounganization.botanify.domain.community.entity.Post;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "Spring")
+public interface PostMapper {
+
+    default Post postReqDtoToPost(PostReqDto postReqDto, Long userId) {
+        return new Post (
+        postReqDto.getTitle(),
+        postReqDto.getContent(),
+        userId);
+    }
+
+    default PostResDto postToPostResDto(Post post, int statusCode, String message) {
+        return new PostResDto(
+                statusCode,
+                message,
+                post.getId()
+        );
+    }
+
+}
