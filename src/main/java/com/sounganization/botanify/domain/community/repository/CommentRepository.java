a695e1f7671162ec9ef7d2d2255631ed33e7e930
soung90 <sounglikane@gmail.com>
2024-12-07 16:26:39 +0900

    feat: Comment Repository methods 구현 #8

--- src/main/java/com/sounganization/botanify/domain/community/repository/CommentRepository.java
+++ src/main/java/com/sounganization/botanify/domain/community/repository/CommentRepository.java
@@ -3,5 +3,10 @@ package com.sounganization.botanify.domain.community.repository;
 import com.sounganization.botanify.domain.community.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostIdAndDeletedYnFalse(Long postId);
+    Optional<Comment> findByIdAndDeletedYnFalse(Long id);
 }
