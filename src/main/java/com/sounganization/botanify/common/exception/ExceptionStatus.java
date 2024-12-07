f838190da9bfa76f054d38a3c33f65082495d7de
soung90 <sounglikane@gmail.com>
2024-12-07 16:27:32 +0900

    fix: 댓글 작성자만 수정, 삭제할 수 있도록 예외 처리 message 수정 #8

--- src/main/java/com/sounganization/botanify/common/exception/ExceptionStatus.java
+++ src/main/java/com/sounganization/botanify/common/exception/ExceptionStatus.java
@@ -35,7 +35,7 @@ public enum ExceptionStatus {
     // comment
     INVALID_COMMENT_CONTENT(HttpStatus.BAD_REQUEST, "댓글 내용을 입력해주세요."),
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다."),
-    NOT_COMMENT_OWNER(HttpStatus.FORBIDDEN, "댓글 작성자만 수정할 수 있습니다.")
+    NOT_COMMENT_OWNER(HttpStatus.FORBIDDEN, "댓글 작성자만 댓글을 수정하거나 삭제할 수 있습니다.")
 
     ;
     private final HttpStatus status;
