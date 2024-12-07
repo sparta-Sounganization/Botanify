14764f866b997c8bc03d5040ba67d1c82ded7848
soung90 <sounglikane@gmail.com>
2024-12-07 12:44:24 +0900

    fix: 댓글의 존재 확인 예외 처리 message 추가 #8

--- src/main/java/com/sounganization/botanify/common/exception/ExceptionStatus.java
+++ src/main/java/com/sounganization/botanify/common/exception/ExceptionStatus.java
@@ -34,6 +34,7 @@ public enum ExceptionStatus {
 
     // comment
     INVALID_COMMENT_CONTENT(HttpStatus.BAD_REQUEST, "댓글 내용을 입력해주세요."),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다.")
 
     ;
     private final HttpStatus status;
