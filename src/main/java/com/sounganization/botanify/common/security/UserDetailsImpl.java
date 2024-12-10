0d06d12c9d39412c576a421cf771e65c6435744f
soung90 <sounglikane@gmail.com>
2024-12-10 17:29:44 +0900

    fix: UserDetailsImpl에 annotations 추가 #8

--- src/main/java/com/sounganization/botanify/common/security/UserDetailsImpl.java
+++ src/main/java/com/sounganization/botanify/common/security/UserDetailsImpl.java
@@ -2,6 +2,8 @@ package com.sounganization.botanify.common.security;
 
 import com.sounganization.botanify.domain.user.dto.req.UserReqDto;
 import com.sounganization.botanify.domain.user.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +12,8 @@ import java.util.Collection;
 import java.util.Collections;
 
 @Getter
+@Builder
+@AllArgsConstructor
 public class UserDetailsImpl implements UserDetails {
 
     private final Long id;
