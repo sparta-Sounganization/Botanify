e180ed26bf99e7917dadeb6c0fc6800e065096fa
soung90 <sounglikane@gmail.com>
2024-12-11 18:02:28 +0900

    fix: 이미 삭제된 댓글의 예외 처리 message 추가 #33

--- src/main/java/com/sounganization/botanify/common/exception/ExceptionStatus.java
+++ src/main/java/com/sounganization/botanify/common/exception/ExceptionStatus.java
@@ -50,7 +50,8 @@ public enum ExceptionStatus {
     // comment
     INVALID_COMMENT_CONTENT(HttpStatus.BAD_REQUEST, "댓글 내용을 입력해주세요."),
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다."),
-    NOT_COMMENT_OWNER(HttpStatus.FORBIDDEN, "댓글 작성자만 댓글을 수정하거나 삭제할 수 있습니다.")
+    NOT_COMMENT_OWNER(HttpStatus.FORBIDDEN, "댓글 작성자만 댓글을 수정하거나 삭제할 수 있습니다."),
+    COMMENT_ALREADY_DELETED(HttpStatus.CONFLICT, "이미 삭제된 댓글입니다.")
 
     ;
     private final HttpStatus status;
