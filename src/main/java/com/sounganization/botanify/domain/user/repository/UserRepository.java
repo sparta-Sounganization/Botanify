8dec8848adb7ec2b14faa3f220b1e0e8d2e83b83
Despereaux <157133321+Despereaux-MAU@users.noreply.github.com>
2024-12-12 10:04:18 +0900

    fix: Conflict 해결 #29

--- src/main/java/com/sounganization/botanify/domain/user/repository/UserRepository.java
+++ src/main/java/com/sounganization/botanify/domain/user/repository/UserRepository.java
@@ -1,10 +1,17 @@
 package com.sounganization.botanify.domain.user.repository;
 
 import com.sounganization.botanify.domain.user.entity.User;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u.username FROM User u WHERE u.id IN :userIds")
+    List<String> findUsernamesByIds(@Param("userIds") Set<Long> userIds);
 }
