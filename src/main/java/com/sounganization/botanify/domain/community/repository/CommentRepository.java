df0a3626261e371c41b61909e5624ab0e7a785ca
JIMINJI1 <mj013031@gmail.com>
2024-12-12 17:06:31 +0900

    feat: findCommentsByPostId queryDsl 적용 #35

--- src/main/java/com/sounganization/botanify/domain/community/repository/CommentRepository.java
+++ src/main/java/com/sounganization/botanify/domain/community/repository/CommentRepository.java
@@ -1,20 +1,7 @@
 package com.sounganization.botanify.domain.community.repository;
 
 import com.sounganization.botanify.domain.community.entity.Comment;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
-public interface CommentRepository extends JpaRepository<Comment, Long>{
-    //게시글 댓글 조회
-    @Query("""
-    SELECT c
-    FROM Comment c
-    WHERE c.post.id = :postId
-    AND c.deletedYn = false
-    ORDER BY COALESCE(c.parentComment.id, c.id), c.id
-""")
-    List<Comment> findCommentsByPostId(@Param("postId") Long postId);
+public interface CommentRepository extends JpaRepository<Comment, Long>, CommentCustomRepository{
     }
