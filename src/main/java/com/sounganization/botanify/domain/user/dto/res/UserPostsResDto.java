ee5ce784959bcd4d0a969f06dad2f885dd45d33e
Despereaux <157133321+Despereaux-MAU@users.noreply.github.com>
2024-12-12 19:01:22 +0900

    fix: 사용되지 않는 import 문 삭제 #29

--- src/main/java/com/sounganization/botanify/domain/user/dto/res/UserPostsResDto.java
+++ src/main/java/com/sounganization/botanify/domain/user/dto/res/UserPostsResDto.java
@@ -1,7 +1,6 @@
 package com.sounganization.botanify.domain.user.dto.res;
 
 import com.sounganization.botanify.domain.community.dto.res.PostListResDto;
-import com.sounganization.botanify.domain.garden.dto.res.DiaryResDto;
 import org.springframework.data.domain.Page;
 
 public record UserPostsResDto(
