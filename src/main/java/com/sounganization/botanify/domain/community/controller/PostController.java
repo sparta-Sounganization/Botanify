566cd994b1536b6a4261040c3fac74b02191becd
soung90 <sounglikane@gmail.com>
2024-12-16 10:54:28 +0900

    feat: 인기글 API URI 수정 #41

--- src/main/java/com/sounganization/botanify/domain/community/controller/PostController.java
+++ src/main/java/com/sounganization/botanify/domain/community/controller/PostController.java
@@ -16,7 +16,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/posts")
+@RequestMapping("/api/v2/posts")
 @RequiredArgsConstructor
 public class PostController {
 
