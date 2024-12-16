456bd94d3f5c5243b61c024a4ee263b770bc5c46
soung90 <sounglikane@gmail.com>
2024-12-16 12:22:00 +0900

    fix: 인기글 API URI 수정 #41

--- src/main/java/com/sounganization/botanify/domain/community/controller/PopularPostController.java
+++ src/main/java/com/sounganization/botanify/domain/community/controller/PopularPostController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v2/posts/popular")
+@RequestMapping("/api/v1/posts/popular")
 @RequiredArgsConstructor
 public class PopularPostController {
 
