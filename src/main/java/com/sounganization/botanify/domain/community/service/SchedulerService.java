a9d9aa6f5c1961e17ef857097626e692e8584748
JIMINJI1 <mj013031@gmail.com>
2024-12-16 22:34:17 +0900

    feat: entityManager.clear 추가 #43

--- src/main/java/com/sounganization/botanify/domain/community/service/SchedulerService.java
+++ src/main/java/com/sounganization/botanify/domain/community/service/SchedulerService.java
@@ -1,6 +1,7 @@
 package com.sounganization.botanify.domain.community.service;
 
 import com.sounganization.botanify.domain.community.repository.ViewHistoryRepository;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,10 +13,12 @@ import org.springframework.stereotype.Service;
 public class SchedulerService {
 
     private final ViewHistoryRepository viewHistoryRepository;
+    private final EntityManager entityManager;
 
     @Scheduled(cron = "0 0 00 L * ?", zone = "Asia/Seoul")
-    public void deleteAll(){
+    public void deleteAll() {
         viewHistoryRepository.deleteAll();
+        entityManager.clear();
         log.info("viewHistory 삭제 스케줄러 동작");
     }
 }
