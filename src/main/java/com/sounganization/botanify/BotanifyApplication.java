8eea25a9d5fbb3392e1699c0ce65497d07ecd5db
JIMINJI1 <mj013031@gmail.com>
2024-12-16 17:37:03 +0900

    feat: 스케줄러 추가 #43

--- src/main/java/com/sounganization/botanify/BotanifyApplication.java
+++ src/main/java/com/sounganization/botanify/BotanifyApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 @EnableJpaAuditing
