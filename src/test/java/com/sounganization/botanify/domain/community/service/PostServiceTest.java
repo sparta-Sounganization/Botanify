2f221308d43e4ed5c74df9181839147372403570
34-43 <jehyuck3443@gmail.com>
2024-12-17 15:35:54 +0900

    feat: 'Post' 3-Layer 구현 및 서비스 코드 변경사항 반영 #38

--- src/test/java/com/sounganization/botanify/domain/community/service/PostServiceTest.java
+++ src/test/java/com/sounganization/botanify/domain/community/service/PostServiceTest.java
@@ -3,13 +3,16 @@ package com.sounganization.botanify.domain.community.service;
 import com.sounganization.botanify.common.dto.res.CommonResDto;
 import com.sounganization.botanify.domain.community.dto.req.PostReqDto;
 import com.sounganization.botanify.domain.community.dto.req.PostUpdateReqDto;
+import com.sounganization.botanify.domain.community.dto.res.CommentTempDto;
 import com.sounganization.botanify.domain.community.dto.res.PostListResDto;
+import com.sounganization.botanify.domain.community.dto.res.PostWithCommentResDto;
 import com.sounganization.botanify.domain.community.entity.Comment;
 import com.sounganization.botanify.domain.community.entity.Post;
 import com.sounganization.botanify.domain.community.mapper.PostMapper;
 import com.sounganization.botanify.domain.community.repository.CommentRepository;
 import com.sounganization.botanify.domain.community.repository.PostRepository;
 import com.sounganization.botanify.domain.user.entity.User;
+import com.sounganization.botanify.domain.user.projection.UserProjection;
 import com.sounganization.botanify.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,6 +22,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.*;
 import org.springframework.http.HttpStatus;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -38,19 +42,54 @@ class PostServiceTest {
     @Mock private CommentRepository commentRepository;
     @Mock private UserRepository userRepository;
     @Mock private PostMapper postMapper;
+    @Mock private PopularPostService popularPostService;
+
+    // 종속 계층 - 조회수 어뷰징 관련
+    @Mock private ViewHistoryRedisService viewHistoryRedisService;
+
+
+    private PostReqDto postReqDto;
+    private Long userId;
+    private Post post;
+    private Post savedPost;
+    private final List<Comment> replies = new ArrayList<>();
+    private final List<Comment> thread = new ArrayList<>();
+    private final List<Long> userIds = new ArrayList<>();
+    private final List<UserProjection> userProjections = new ArrayList<>();
+    private final List<CommentTempDto> commentDtos = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        postReqDto = new PostReqDto("test title", "test content");
+        userId = 1L;
+        post = mock(Post.class);
+        savedPost = Post.builder().id(1L).build();
+
+        Comment childComment = Comment.builder().id(2L).build();
+        replies.add(childComment);
+        Comment parentComment = Comment.builder().id(1L).userId(1L).childComments(replies).build();
+        thread.add(parentComment);
+
+        userIds.add(userId);
+        userProjections.add(new UserProjection() {
+            @Override
+            public Long getId() {
+                return userId;
+            }
+
+            @Override
+            public String getUsername() {
+                return "test user";
+            }
+        });
+
+        commentDtos.add(new CommentTempDto(1L,userId,"test user",null));
     }
 
     @Test
     void createPost_Success() {
         // given
-        PostReqDto postReqDto = new PostReqDto("test title", "test content");
-        Long userId = 1L;
-        Post post = new Post();
-        Post savedPost = Post.builder().id(1L).build();
         when(userRepository.findById(userId)).thenReturn(Optional.of(mock(User.class)));
         when(postMapper.reqDtoToEntity(postReqDto, userId)).thenReturn(post);
         when(postRepository.save(post)).thenReturn(savedPost);
@@ -91,13 +130,27 @@ class PostServiceTest {
 
     @Test
     void readPost_Success() {
-        // todo - 작성 중
         // given
         Long postId = 1L;
-        // when
-        postService.readPost(postId);
-        // then
+        Long userId = 1L;
+        when(postRepository.findById(postId)).thenReturn(Optional.of(post));
+        doNothing().when(popularPostService).updatePostScore(postId);
+        when(commentRepository.findCommentsByPostId(postId)).thenReturn(thread);
+        when(userRepository.findUsernamesByIds(userIds)).thenReturn(userProjections);
+        when(postMapper.entityToResDto(eq(post), eq(commentDtos)))
+                .thenReturn(mock(PostWithCommentResDto.class));
+        when(viewHistoryRedisService.isViewHistoryExist(anyLong(),anyLong(),any(LocalDate.class)))
+                .thenReturn(false);
 
+        // when
+        PostWithCommentResDto result = postService.readPost(postId, userId);
+        // then
+        assertNotNull(result);
+        verify(postRepository).findById(postId);
+        verify(popularPostService).updatePostScore(postId);
+        verify(commentRepository).findCommentsByPostId(postId);
+        verify(userRepository).findUsernamesByIds(userIds);
+        verify(postMapper).entityToResDto(eq(post), eq(commentDtos));
     }
 
     @Test
