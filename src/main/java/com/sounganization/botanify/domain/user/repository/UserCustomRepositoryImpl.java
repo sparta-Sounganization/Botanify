5e511f85281997f51e5c02fc0652d08b35b468b2
Despereaux <157133321+Despereaux-MAU@users.noreply.github.com>
2024-12-17 17:28:17 +0900

    refactor: `findUsernamesByIds`, `updateUserInfo` QueryDsl로 리팩토링 #53

---
+++ src/main/java/com/sounganization/botanify/domain/user/repository/UserCustomRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.sounganization.botanify.domain.user.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sounganization.botanify.domain.user.entity.QUser;
+import com.sounganization.botanify.domain.user.projection.UserProjection;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class UserCustomRepositoryImpl implements UserCustomRepository{
+
+    private final JPAQueryFactory jpaQueryFactory;
+    private final QUser user = QUser.user;
+
+    @Override
+    public List<UserProjection> findUsernamesByIds(List<Long> userIds) {
+        return jpaQueryFactory.select(Projections.fields(UserProjection.class,
+                user.id.as("id"),
+                user.username.as("username")
+        ))
+                .from(user)
+                .where(user.id.in(userIds))
+                .fetch();
+    }
+
+    @Override
+    public void updateUserInfo(Long id, String username, String password, String city, String town, String address) {
+        jpaQueryFactory.update(user)
+                .set(user.username, username)
+                .set(user.password, password)
+                .set(user.city, city)
+                .set(user.town, town)
+                .set(user.address, address)
+                .where(user.id.eq(id))
+                .execute();
+    }
+}
--- src/main/java/com/sounganization/botanify/domain/user/repository/UserRepository.java
+++ src/main/java/com/sounganization/botanify/domain/user/repository/UserRepository.java
@@ -1,30 +1,10 @@
 package com.sounganization.botanify.domain.user.repository;
 
 import com.sounganization.botanify.domain.user.entity.User;
-import com.sounganization.botanify.domain.user.projection.UserProjection;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserCustomRepository {
     Optional<User> findByEmail(String email);
-
-    //username 찾기
-    @Query("SELECT u.id AS id, u.username AS username FROM User u WHERE u.id IN :userIds")
-    List<UserProjection> findUsernamesByIds(@Param("userIds") List<Long> userIds);
-
-    @Modifying
-    @Query("UPDATE User u SET u.username = :username," +
-            " u.password = :password, u.city = :city, u.town = :town, u.address = :address" +
-            " WHERE u.id = :id")
-    void updateUserInfo(@Param("id") Long id,
-                        @Param("username") String username,
-                        @Param("password") String password,
-                        @Param("city") String city,
-                        @Param("town") String town,
-                        @Param("address") String address);
 }
