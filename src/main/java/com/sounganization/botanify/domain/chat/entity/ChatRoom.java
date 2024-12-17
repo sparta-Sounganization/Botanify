92f5b993ebb6983b9b163d06e7ccaf52c86806ba
soung90 <sounglikane@gmail.com>
2024-12-17 14:25:01 +0900

    feat: 채팅 세션을 관리하는 ChatRoom entity 구현 #50

---
+++ src/main/java/com/sounganization/botanify/domain/chat/entity/ChatRoom.java
@@ -0,0 +1,31 @@
+package com.sounganization.botanify.domain.chat.entity;
+
+import com.sounganization.botanify.common.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatRoom extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long senderUserId;
+
+    @Column(nullable = false)
+    private Long receiverUserId;
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL)
+    private List<ChatMessage> messages = new ArrayList<>();
+}
