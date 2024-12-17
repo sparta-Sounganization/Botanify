1b77fb8768ee1394c3bf93120be4a00791b28099
JIMINJI1 <mj013031@gmail.com>
2024-12-17 12:07:03 +0900

    remove: dev 병합 후 중복 어노테이션 삭제 #43

--- src/main/java/com/sounganization/botanify/BotanifyApplication.java
+++ src/main/java/com/sounganization/botanify/BotanifyApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
+
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 @EnableJpaAuditing
