174c5ae0a74c604aa43fe90598402cae3ba89c9a
soung90 <sounglikane@gmail.com>
2024-12-17 21:59:13 +0900

    fix: WebSocket 연동 적용 #50

--- src/main/java/com/sounganization/botanify/common/config/websocket/WebSocketConfig.java
+++ src/main/java/com/sounganization/botanify/common/config/websocket/WebSocketConfig.java
@@ -16,11 +16,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(chatWebSocketHandler, "/ws/chat")
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*");
     }
-
-//    @Bean
-//    public WebSocketHandler chatWebSocketHandler() {
-//        return new ChatWebSocketHandler();
-//    }
 }
