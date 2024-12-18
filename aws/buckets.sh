1db11550d4386e412322a6c0f125c72c8b395752
34-43 <jehyuck3443@gmail.com>
2024-12-18 14:31:58 +0900

    chore: localstack(local s3) environment 개선 #48

--- aws/buckets.sh
+++ aws/buckets.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+
+# S3 서비스가 준비될 때까지 기다리기
+until awslocal s3 ls > /dev/null 2>&1; do
+  echo "Waiting for S3 service to be ready..."
+  sleep 2
+done
+
+# 버킷 생성
 awslocal s3 mb s3://botanify-backend-bucket
