64148bed97b9372364ec5f0bbaa9cba31e7ba622
34-43 <jehyuck3443@gmail.com>
2024-12-18 14:52:15 +0900

    chore: localstack(local s3) environment 개선 및 CORS 설정 추가 #48

--- aws/01-create-bucket.sh
+++ aws/01-create-bucket.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# docker-compose 설정에 의해, localstack 이미지의 시작과 함께 실행되는 스크립트.
+# 지정된 명칭의 버킷을 생성한다.
+
 # S3 서비스가 준비될 때까지 기다리기
 until awslocal s3 ls > /dev/null 2>&1; do
   echo "Waiting for S3 service to be ready..."
