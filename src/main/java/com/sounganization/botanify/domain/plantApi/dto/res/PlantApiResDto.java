82303391182c4bc1ef0caacd528bbe618e68913d
JIMINJI1 <mj013031@gmail.com>
2024-12-24 12:24:10 +0900

    feat: 식물 api dto 추가 #55

---
+++ src/main/java/com/sounganization/botanify/domain/plantApi/dto/res/PlantApiResDto.java
@@ -0,0 +1,23 @@
+package com.sounganization.botanify.domain.plantApi.dto.res;
+
+import lombok.Builder;
+
+@Builder
+public record PlantApiResDto(
+        String codeNm,               //품종명
+        String cntntsNo,             // 식물 번호
+        String cntntsSj,             // 식물 이름
+        String smell,                // 냄새
+        String toxicity,             // 독성 정보
+        String managementLevel,      // 관리 수준
+        String growthSpeed,          // 생장 속도
+        String growthTemperature,    // 생육 온도
+        String winterLowestTemp,     // 겨울 최저 온도
+        String humidity,             // 습도
+        String fertilizerInfo,       // 비료 정보
+        String waterSpring,          // 봄 물주기
+        String waterSummer,          // 여름 물주기
+        String waterAutumn,          // 가을 물주기
+        String waterWinter           // 겨울 물주기
+) {}
+
