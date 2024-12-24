73b7e6cd57fd4a74c89043fe0b034a878f568e48
JIMINJI1 <mj013031@gmail.com>
2024-12-24 12:25:20 +0900

    feat: webclient config 추가 #55

---
+++ src/main/java/com/sounganization/botanify/common/config/webClient/WebClientConfig.java
@@ -0,0 +1,28 @@
+package com.sounganization.botanify.common.config.webClient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.xml.Jaxb2XmlDecoder;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+@Configuration
+public class WebClientConfig {
+    @Value("${spring.nongsaro.api.base-url}")
+    private String baseUrl;
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.TEXT_XML_VALUE)
+                .codecs(configurer -> {
+                    configurer.defaultCodecs().jaxb2Decoder(new Jaxb2XmlDecoder());
+                })
+                .build();
+    }
+}
+
