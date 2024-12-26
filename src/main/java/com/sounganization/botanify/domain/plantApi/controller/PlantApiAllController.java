30f0370bf81aaaed25b33c6239222734305f09f4
34-43 <jehyuck3443@gmail.com>
2024-12-26 16:23:03 +0900

    feat: 스케줄러 수동 실행 API 추가 #55

--- src/main/java/com/sounganization/botanify/domain/plantApi/controller/PlantApiAllController.java
+++ src/main/java/com/sounganization/botanify/domain/plantApi/controller/PlantApiAllController.java
@@ -1,8 +1,12 @@
 package com.sounganization.botanify.domain.plantApi.controller;
 
+import com.sounganization.botanify.common.dto.res.CommonResDto;
+import com.sounganization.botanify.common.service.SchedulerService;
 import com.sounganization.botanify.domain.plantApi.dto.res.PlantApiResDto;
 import com.sounganization.botanify.domain.plantApi.service.PlantApiAllService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -14,11 +18,18 @@ import java.util.List;
 @RestController
 public class PlantApiAllController {
     private final PlantApiAllService apiService;
+    private final SchedulerService schedulerService;
 
     //전체 품종의 모든 식물의 상세 정보 조회
     @GetMapping("/api/plant-api/all")
     public Mono<List<PlantApiResDto>> getGrowthForms() {
         return apiService.getSpeciesWithDetails()
                 .switchIfEmpty(Mono.just(Collections.emptyList()));
+    }
+
+    @GetMapping("/api/plant-api/update")
+    public ResponseEntity<CommonResDto> manualUpdate() {
+        schedulerService.updateSpeciesTableFromAPI().block();
+        return ResponseEntity.ok(new CommonResDto(HttpStatus.OK,"실행 완료."));
     }
 }
