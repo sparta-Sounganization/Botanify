212085e80f1d71128348fce29355acaac8c00ab9
soung90 <sounglikane@gmail.com>
2024-12-31 16:45:54 +0900

    fix: PlantAlarmController에 CommonResDto 적용 #80

--- src/main/java/com/sounganization/botanify/domain/garden/controller/PlantAlarmController.java
+++ src/main/java/com/sounganization/botanify/domain/garden/controller/PlantAlarmController.java
@@ -1,5 +1,6 @@
 package com.sounganization.botanify.domain.garden.controller;
 
+import com.sounganization.botanify.common.dto.res.CommonResDto;
 import com.sounganization.botanify.common.security.UserDetailsImpl;
 import com.sounganization.botanify.domain.garden.dto.req.PlantAlarmReqDto;
 import com.sounganization.botanify.domain.garden.dto.req.PlantAlarmUpdateReqDto;
@@ -7,6 +8,7 @@ import com.sounganization.botanify.domain.garden.dto.res.PlantAlarmResDto;
 import com.sounganization.botanify.domain.garden.service.PlantAlarmService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -20,13 +22,17 @@ public class PlantAlarmController {
     private final PlantAlarmService plantAlarmService;
 
     @PostMapping("/{plantId}/alarms")
-    public ResponseEntity<Long> createAlarm(
+    public ResponseEntity<CommonResDto> createAlarm(
             @PathVariable Long plantId,
             @RequestBody @Valid PlantAlarmReqDto reqDto,
             @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
         Long alarmId = plantAlarmService.createAlarm(userDetails.getId(), plantId, reqDto);
-        return ResponseEntity.ok(alarmId);
+        return ResponseEntity.ok(new CommonResDto(
+                HttpStatus.CREATED,
+                "알람이 성공적으로 생성되었습니다.",
+                alarmId
+        ));
     }
 
     @GetMapping("/alarms")
@@ -47,13 +53,16 @@ public class PlantAlarmController {
     }
 
     @PutMapping("/alarms/{alarmId}")
-    public ResponseEntity<Void> updateAlarm(
+    public ResponseEntity<CommonResDto> updateAlarm(
             @PathVariable Long alarmId,
             @RequestBody @Valid PlantAlarmUpdateReqDto reqDto,
             @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
         plantAlarmService.updateAlarm(userDetails.getId(), alarmId, reqDto);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new CommonResDto(
+                HttpStatus.OK,
+                "알람이 성공적으로 수정되었습니다."
+        ));
     }
 
     @DeleteMapping("/alarms/{alarmId}")
