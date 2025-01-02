098bac206eb1e16567eb9dbdaf1c9423aca01a78
arago0712 <arago0712@gmail.com>
2025-01-02 19:13:59 +0900

    keumsunghyun 님이 변경한 내용

--- src/main/java/com/sounganization/botanify/common/config/SecurityConfig.java
+++ src/main/java/com/sounganization/botanify/common/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.sounganization.botanify.domain.user.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
