49e166b445acc98d1ef7a45837ec25916bad5890
soung90 <sounglikane@gmail.com>
2025-01-15 12:25:05 +0900

    fix: 메시지 비동기적으로 저장하기 위해 설정 #90

---
+++ src/main/java/com/sounganization/botanify/common/config/async/AsyncConfig.java
@@ -0,0 +1,36 @@
+package com.sounganization.botanify.common.config.async;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+@Slf4j
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Bean(name = "chatThreadPoolTaskExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.setThreadNamePrefix("ChatAsync-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (ex, method, params) -> {
+            log.error("비동기 메서드 {}에서 예외 발생: {}", method.getName(), ex.getMessage());
+        };
+    }
+}
